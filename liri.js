9f17a434dbe2e2c9c44c5c2bd000ea3074b041a2
Relliner <41902228+rehlingera@users.noreply.github.com>
2018-12-17 18:27:11 -0600

    updated the overall look and functionality

--- liri.js
+++ liri.js
@@ -1,68 +1,239 @@
+//Requiring all the necessary toos and documents...
 require("dotenv").config();
 var keys = require("./keys.js");
-
 var axios = require("axios");
-
+var inquirer = require("inquirer");
 var Spotify = require('node-spotify-api');
-
 var moment = require("moment");
-
 var fs = require("fs");
 
-var command = process.argv[2];
+//Establishing the input variable on the global scope...
 var input = "";
 
+//Populating the "Liri.js" welcome message...
+console.log("\n██╗     ██╗██████╗ ██╗        ██╗███████╗\n██║     ██║██╔══██╗██║        ██║██╔════╝\n██║     ██║██████╔╝██║        ██║███████╗\n██║     ██║██╔══██╗██║   ██   ██║╚════██║\n███████╗██║██║  ██║██║██╗╚█████╔╝███████║\n╚══════╝╚═╝╚═╝  ╚═╝╚═╝╚═╝ ╚════╝ ╚══════╝");
+console.log("\nWelcome to the LIRI Bot!\n");
+
+//Function to begin the program...
+start = function () {
+    //Begin inquirer...
+    inquirer.prompt([
+        //Starting command selection...
+        {
+            type: "list",
+            name: "command",
+            choices: ["Search for concerts", "Spotify a song", "Look up a movie", "Random command", "Exit"],
+            message: "What would you like to do?"
+        }
+    ]).then(function (answers) {
+        //If the user picks the concert function, ask for a band/artist name...
+        if (answers.command === "Search for concerts") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "input",
+                    message: "Enter band/artist name:"
+                }
+            ]).then(function (answers) {
+                //...then execute the concert function with the name supplied...
+                input = answers.input;
+                concert(input);
+            });
+        }
+        //If the user picks the Spotify function, ask for a song title...
+        else if (answers.command === "Spotify a song") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "input",
+                    message: "Enter song title:"
+                }
+            ]).then(function (answers) {
+                //...then execute the Spotify function with the title supplied...
+                input = answers.input;
+                spot(input);
+            });
+        }
+        //If the user picks the movie function, ask for a movie title...
+        else if (answers.command === "Look up a movie") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "input",
+                    message: "Enter movie title:"
+                }
+            ]).then(function (answers) {
+                //...then execute the movie function with the title supplied...
+                input = answers.input;
+                movie(input);
+            });
+        }
+        //If the user picks "random command," first access the random.txt file and pull the data...
+        else if (answers.command === "Random command") {
+            fs.readFile("random.txt", "utf-8", function (err, data) {
+                if (err) {
+                    console.log("There has been an error. Please try again.");
+                };
+                //...then split it into a table at the commas...
+                var randomArray = data.split(",");
+
+                //Generate random number with a max of the array length/2...
+                var randomInt = Math.floor(Math.random() * (randomArray.length / 2));
+
+                //Multiply the random number by 2 for the command, then log that command to the console for the user to see...
+                com = randomInt * 2;
+                console.log("Command: " + randomArray[com])
+
+                //Add 1 to the command for the input, then log that input to the console for the user to see...
+                inp = +com + 1;
+                console.log("Input: " + randomArray[inp] + "\n==========");
+
+                //If the command is "concert-this," execute the concert function with the input above...
+                if (randomArray[com] === "concert-this") {
+                    command = randomArray[com];
+                    input = randomArray[inp];
+                    concert(input);
+                }
+                //If the command is "spotify-this-song," execute the Spotify function with the input above...
+                else if (randomArray[com] === "spotify-this-song") {
+                    command = randomArray[com];
+                    input = randomArray[inp];
+                    spot(input);
+                }
+                //If the command is "movie-this," execute the movie function with the input above...
+                else if (randomArray[com] === "movie-this") {
+                    command = randomArray[com];
+                    input = randomArray[inp];
+                    movie(input);
+                };
+            });
+        }
+        //If the user picks the exit function, execute the end function...
+        else if (answers.command === "Exit") {
+            end();
+        };
+    });
+};
+
+//The end function:
+end = function () {
+    //Print the goodbye message to the console...
+    console.log("\n██████╗██╗ ██╗ ████╗ ███╗  ██╗██╗ ██╗     ██╗ ██╗ █████╗ ██╗  ██╗██╗\n╚═██╔═╝██║ ██║██╔═██╗████╗ ██║██║██╔╝     ╚████╔╝██╔══██╗██║  ██║██║\n  ██║  ██████║██████║██╔██╗██║████╔╝       ╚██╔╝ ██║  ██║██║  ██║╚═╝\n  ██║  ██╔═██║██╔═██║██║╚████║██╔═██╗       ██║  ╚█████╔╝╚█████╔╝██╗\n  ╚═╝  ╚═╝ ╚═╝╚═╝ ╚═╝╚═╝ ╚═══╝╚═╝ ╚═╝       ╚═╝   ╚════╝  ╚════╝ ╚═╝");
+    console.log("\nThank you for using LIRI Bot!\n");
+};
+
+//The concert function...
 var concert = function (input) {
-    var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+    //Establish the URL for the API, plugging in the unput passed in...
+    var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+    //Call the API using axios...
     axios.get(queryUrl).then(
         function (response) {
+            //If the data is empty, print the "no results" message to the console...
             if (response.data.length === 0) {
                 console.log("Sorry, no concert results for this band!");
             };
+            //Establish the divider and responseData variables...
             var divider = "\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n\n";
             var responseData = [];
+            //For each result, until the number of results is exhausted...
             for (i = 0; i < response.data.length; i++) {
+                //...make an array containing the venue, location and date, with a line to break it away from the other results, and join it with a few break spaces...
                 anotherResponse = [
                     "\nVenue: " + response.data[i].venue.name,
                     "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country,
                     "Date: " + moment(response.data[i].datetime).format('MM/DD/YYYY'),
                     "----------"
                 ].join("\n\n");
+                //...and push it to the responseData array...
                 responseData.push(anotherResponse);
             };
-            fs.appendFile("log.txt", "Command: " + command + "\nInput: " + input + "\n\n" + responseData.join("\n") + divider, function (err) {
+            //Access the "log.txt" file and add responseData to it...
+            fs.appendFile("log.txt", "Command: " + "concert-this" + "\nInput: " + input + "\n\n" + responseData.join("\n") + divider, function (err) {
+                //If there is an error, tell the user...
                 if (err) throw err;
-            console.log(responseData.join("\n"));
+                //Print response Data to the console for the user to see...
+                console.log(responseData.join("\n"));
+                //Ask the user to press Enter to continue, because the inquirer prompt prints above the results and may not be seen...
+                console.log("Press Enter to continue");
             });
         }
     );
+    //Ask the user to press Enter to continue...
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Press Enter to continue"
+        }
+    ]).then(function (answers) {
+        //If the user presses enter, execute the start function again...
+        if (answers.continue) {
+            start();
+        }
+        //Otherwise, say good-bye by executing the end function...
+        else {
+            end();
+        };
+    });
 };
 
+//The Spotify function...
 var spot = function (input) {
+    //Make a new key and secret object with the constructor from the node-spotify-api...
     var sptfy = new Spotify({
+        //Pull the id and secret from the keys file...
         id: keys.spotify.id,
         secret: keys.spotify.secret
     });
+    //Perform a Spotify search using the input provided...
     sptfy.search({ type: 'track', query: input }, function (err, data) {
+        //If there has been an error, tell the user...
         if (err) {
             return console.log('Error occurred: ' + err);
         };
+        //Establish the divider and responseData variables...
         var divider = "\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n\n";
         var responseData = [];
+        //For each result, until the number of results is exhausted...
         for (i = 0; i < data.tracks.items.length; i++) {
+            //...establish an array with the artist, song title, preview link and album, with a line to break it away from the other results, and join it with a few break spaces...
             anotherResponse = [
-            "\nArtist(s): " + data.tracks.items[i].artists[0].name,
-            "Song: " + data.tracks.items[i].name,
-            "Preview: " + data.tracks.items[i].preview_url,
-            "Album: " + data.tracks.items[i].album.name,
-            "----------",
+                "\nArtist(s): " + data.tracks.items[i].artists[0].name,
+                "Song: " + data.tracks.items[i].name,
+                "Preview: " + data.tracks.items[i].preview_url,
+                "Album: " + data.tracks.items[i].album.name,
+                "----------",
             ].join("\n\n");
+            //...then push it the the responseData array...
             responseData.push(anotherResponse);
         };
-        fs.appendFile("log.txt", "Command: " + command + "\nInput: " + input + "\n\n" + responseData.join("\n") + divider, function (err) {
+        //Access the "log.txt" file and add responseData to it...
+        fs.appendFile("log.txt", "Command: " + "spotify-this-song" + "\nInput: " + input + "\n\n" + responseData.join("\n") + divider, function (err) {
+            //If there is an error, ell the user...
             if (err) throw err;
-        console.log(responseData.join("\n"));
+            //Print the responseData to the console for the user to see...
+            console.log(responseData.join("\n"));
+            //Ask the user to press Enter to continue, because the inquirer prompt prints above the results and may not be seen...
+            console.log("Press Enter to continue");
         });
+    });
+    //Ask the user to press Enter to continue...
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Press Enter to continue"
+        }
+    ]).then(function (answers) {
+        //If the user presses Enter, exeute the start function again...
+        if (answers.continue) {
+            start();
+        }
+        //Otherwise, say good-bye with the end function...
+        else {
+            end();
+        };
     });
 };
 
@@ -86,64 +257,27 @@ var movie = function (input) {
             "Plot: " + response.data.Plot,
             "Actors: " + response.data.Actors
         ].join("\n\n");
-        fs.appendFile("log.txt", "Command: " + command + "\nInput: " + input + "\n\n" + responseData + divider, function (err) {
+        fs.appendFile("log.txt", "Command: " + "movie-this" + "\nInput: " + input + "\n\n" + responseData + divider, function (err) {
             if (err) throw err;
             console.log(responseData);
+            console.log("Press Enter to continue");
         });
     });
-}
-
-if (command === "concert-this") {
-    input = process.argv.slice(3).join(" ");
-    concert(input);
-}
-
-else if (command === "spotify-this-song") {
-    input = process.argv.slice(3).join(" ");
-    spot(input);
-}
-
-else if (command === "movie-this") {
-    input = process.argv.slice(3).join(" ");
-    movie(input);
-}
-
-else if (command === "do-what-it-says") {
-    fs.readFile("random.txt", "utf-8", function (err, data) {
-        if (err) {
-            console.log("There has been an error. Please try again.");
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Press Enter to continue"
+        }
+    ]).then(function (answers) {
+        if (answers.continue) {
+            start();
+        }
+        else {
+            console.log("Thank you for using LIRI Bot!");
         };
-        var randomArray = data.split(",");
+    });
+};
 
-        //Generate random number with a max of the array length/2
-        var randomInt = Math.floor(Math.random() * (randomArray.length / 2));
-
-        //multiply the random number by 2 for the command
-        com = randomInt * 2;
-        console.log("Command: " + randomArray[com])
-
-        //add 1 to the command for the input
-        inp = +com + 1;
-        console.log("Input: " + randomArray[inp] + "\n==========");
-
-        if (randomArray[com] === "concert-this") {
-            command = randomArray[com];
-            input = randomArray[inp];
-            concert(input);
-        }
-        else if (randomArray[com] === "spotify-this-song") {
-            command = randomArray[com];
-            input = randomArray[inp];
-            spot(input);
-        }
-        else if (randomArray[com] === "movie-this") {
-            command = randomArray[com];
-            input = randomArray[inp];
-            movie(input);
-        }
-    })
-}
-
-else {
-    console.log("Command not recognized. Try one of the following:\nconcert-this [artist/band name]\nspotify-this-song [song name]\nmovie-this [movie title]\ndo-what-it-says");
-}
+//Start the program with the start function...
+start();
